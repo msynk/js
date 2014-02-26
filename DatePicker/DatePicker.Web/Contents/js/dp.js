1aeaa1cd292a3ef2e3dda33dc22367899d03dde7
msynk <msynk@outlook.com>
2014-02-26 11:51:50 +0330

    Initial commit.

---
+++ DatePicker/DatePicker.Web/Contents/js/dp.js
@@ -0,0 +1,13 @@
+﻿var dp = {};
+dp.createDiv = function () {
+  var div = document.createElement('div');
+  div.style.Position = 'absolute';
+  return div;
+};
+dp.div = dp.createDiv();
+dp.render = function (id) {
+  var t = document.getElementById(id),
+    d = dp.div;
+  d.style.top = t.offsetTop + t.offsetHeight + 'px';
+  d.style.left = t.offsetHeight + 'px';
+}
